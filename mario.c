aad70d235b82f9a130d12c178fedc4f411e3724f
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-10-30 19:21:51 +0000

    Wed, Oct 30, 2024, 10:21 PM +03:00

--- mario.c
+++ mario.c
@@ -1,11 +1,13 @@
-#include<cs50.h>
-#include<stdio.h>
+#include <cs50.h>
+#include <stdio.h>
 
-int main(void){
-    int n=get_int("Howmany blocks do you want? ");
-    for(int i=0;i<n;i++){
-        for(int j=1; j<i+1;j++)
-        printf("#");
+int main(void)
+{
+    int n = get_int("Howmany blocks do you want? ");
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < i + 1; j++)
+            printf("#");
         printf("\n");
     }
 }

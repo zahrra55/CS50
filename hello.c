c78fe7cb334c7ea322d4708644608aa39459db7b
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-10-30 19:22:43 +0000

    Wed, Oct 30, 2024, 10:22 PM +03:00

--- hello.c
+++ hello.c
@@ -3,7 +3,6 @@
 int main(void)
 {
     string name = get_string("What's your name? ");
-    //printf("Hello, %s\n", name);
     int age = get_int("How old are you? ");
     string phone = get_string("What's your phone number? ");
     printf("Hello,%s\nYou are %i years old\nYour phone number is: %s \n ",name,age,phone);

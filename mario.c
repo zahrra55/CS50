63f1dcb35f60f28c9e11746321eab2b3b6e40203
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-10-30 19:32:10 +0000

    Wed, Oct 30, 2024, 10:32 PM +03:00

--- mario.c
+++ mario.c
@@ -14,7 +14,7 @@ int main(void)
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = n-1 ; j < i - 1 && i >= 0 ; j++)
+        for (int j = 1; j < i + 1; j++)
             printf("#");
         printf("\n");
     }

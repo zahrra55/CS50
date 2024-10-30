35e1370a07618f616dfb781cf6a40777ed52c1c6
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-10-30 13:57:52 +0000

    Wed, Oct 30, 2024, 4:57 PM +03:00

--- mario.c
+++ mario.c
@@ -3,7 +3,11 @@
 
 int main(void){
     int n=get_int("Howmany blocks do you want? ");
-    for(int i=0;i<n;i++)
-        for(int j=0; j<n;j++)
-        
+    for(int i=0;i<n;i++){
+       printf("\n");
+        for(int j=0; j<i;j++)
+        printf("#");
+    }
+
+
 }

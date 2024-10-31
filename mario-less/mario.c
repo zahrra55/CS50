58b6698f27856a333dd65ecc78dbf5bfdacd063a
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-10-31 17:44:48 +0000

    Thu, Oct 31, 2024, 8:44 PM +03:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -10,7 +10,7 @@ int main(void)
         n = get_int("Height: ");
         s = get_int("Spaces: ");
     }
-    while(n < 2 || n > 7);
+    while(n < 2 && n > 7);
 
     //for(int j = 0; j < d; j++)
     for(int i = 0; i < n; i++)

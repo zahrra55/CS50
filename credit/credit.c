533b00855668f261cd88b00750d5a8e99d2bbefe
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-01 17:46:30 +0000

    Fri, Nov 1, 2024, 8:46 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -11,34 +11,35 @@ int main(void)
     {
         Cnumber = get_long("Number: ");
     }
-    while(Cnumber < 0);
+    while (Cnumber < 0);
 
-    if(Luhn_s_Algorithm(Cnumber))
-    {//if lohn algorithm is right
-        //check the card type
+    if (Luhn_s_Algorithm(Cnumber))
+    { // if lohn algorithm is right
+        // check the card type
         long s = Cnumber;
-        while(s > 0)
+        while (s > 0)
         {
             s /= 10;
             len++;
         }
         long fir_sec_dgts = Cnumber;
-        while(fir_sec_dgts >= 100)
+        while (fir_sec_dgts >= 100)
         {
             fir_sec_dgts /= 10;
         }
-        if(len == 15 && (fir_sec_dgts == 34 || fir_sec_dgts == 37 ))
+        if (len == 15 && (fir_sec_dgts == 34 || fir_sec_dgts == 37))
             printf("AMEX\n");
 
-        else if(len == 16 && (fir_sec_dgts >= 51 && fir_sec_dgts <= 55 ))
+        else if (len == 16 && (fir_sec_dgts >= 51 && fir_sec_dgts <= 55))
             printf("MASTERCARD\n");
 
-        else if((len == 13 || len == 16) && (fir_sec_dgts / 10 == 4 ))
+        else if ((len == 13 || len == 16) && (fir_sec_dgts / 10 == 4))
             printf("VISA\n");
         else
-        printf("INVALID\n");
+            printf("INVALID\n");
     }
-    else{
+    else
+    {
         printf("INVALID\n");
     }
 }
@@ -46,12 +47,12 @@ int main(void)
 bool Luhn_s_Algorithm(long n)
 {
     int sum = 0, pos = 0;
-    while(n > 0)
+    while (n > 0)
     {
-        int digit = n % 10; //finding the last digit
-        if(pos % 2 == 1)
+        int digit = n % 10; // finding the last digit
+        if (pos % 2 == 1)
         {
-            digit *= 2; // multiplying every other digit by 2
+            digit *= 2;                         // multiplying every other digit by 2
             sum += (digit / 10) + (digit % 10); // if more than 10, make 2 digits of it
         }
         else
@@ -59,7 +60,7 @@ bool Luhn_s_Algorithm(long n)
             sum += digit; // adding the rest of the digits
         }
         n /= 10; // complete the process
-        pos++; // increse the pos value
+        pos++;   // increse the pos value
     }
-  return (sum % 10 ) == 0;
+    return (sum % 10) == 0;
 }

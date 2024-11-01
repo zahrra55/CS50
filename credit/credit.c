77385398dcde81b9fc56c35371e7ae385bd9aead
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-01 17:00:10 +0000

    Fri, Nov 1, 2024, 8:00 PM +03:00

--- credit/credit.c
+++ credit/credit.c
@@ -25,6 +25,10 @@ bool Luhn_s_Algorithm(long n)
     while(n > 0)
     {
         int digit = n % 10; //finding the last digit
-        
+        if(pos % 2 == 1)
+        {
+            digit *= 2; // multiplying every other digit by 2
+            sum += (digit / 10) + (digit % 10); // if more than 2 make 
+        }
     }
 }

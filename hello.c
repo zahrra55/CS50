ca8a5fbd4d1a8e25bdf6bebc929738fc4c186b08
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-04 19:07:17 +0000

    Mon, Nov 4, 2024, 10:07 PM +03:00

--- hello.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<cs50.h>
-int main(void)
-{
-    string name = get_string("What's your name? ");
-    int age = get_int("How old are you? ");
-    string phone = get_string("What's your phone number? ");
-    printf("Hello,%s\nYou are %i years old\nYour phone number is: %s \n ",name,age,phone);
-}
---
+++ scrabble/scrabble.c
@@ -0,0 +1,12 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string p1 = get_string("Player 1 : ");
+    string p2 = get_string("Player 2 : ");
+    for(int i = 0; i < )
+
+}

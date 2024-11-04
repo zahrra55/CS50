e9bec259b8c8b149f2285353dbdaf9ad182168d5
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-04 19:11:00 +0000

    Mon, Nov 4, 2024, 10:11 PM +03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -7,6 +7,12 @@ int main(void)
 {
     string p1 = get_string("Player 1 : ");
     string p2 = get_string("Player 2 : ");
-    for(int i = 0; i < )
+    int p1_len = strlen(p1);
+    int p1_len = strlen(p1);
+
+    for(int i = 0; i < p1_len; i++)
+    {
+        if(p1[i] == 'A')
+    }
 
 }

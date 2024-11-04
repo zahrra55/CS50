8a3d8da80f9f5748de035722181e137109a34c52
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-04 20:56:50 +0000

    Mon, Nov 4, 2024, 11:56 PM +03:00

---
+++ readability/readability.c
@@ -0,0 +1,17 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    // Prompt the user for some text
+    string text = get_string("Text: ");
+    
+    // Count the number of letters, words, and sentences in the text
+
+    // Compute the Coleman-Liau index
+
+    // Print the grade level
+}

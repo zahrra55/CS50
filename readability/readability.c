30dff4fb0d218f70cae31fe5924a94b1c8cd1c3d
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-04 20:58:27 +0000

    Mon, Nov 4, 2024, 11:58 PM +03:00

--- readability/readability.c
+++ readability/readability.c
@@ -4,14 +4,36 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
+
 int main(void)
 {
     // Prompt the user for some text
     string text = get_string("Text: ");
-    
+
     // Count the number of letters, words, and sentences in the text
+    int letters = count_letters(text);
+    int words = count_words(text);
+    int sentences = count_sentences(text);
 
     // Compute the Coleman-Liau index
 
     // Print the grade level
+}
+
+int count_letters(string text)
+{
+    // Return the number of letters in text
+}
+
+int count_words(string text)
+{
+    // Return the number of words in text
+}
+
+int count_sentences(string text)
+{
+    // Return the number of sentences in text
 }

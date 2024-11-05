46e47f28b2ec1f75c0f678467628d152b631afe9
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-05 16:28:28 +0000

    Tue, Nov 5, 2024, 7:28 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -8,15 +8,17 @@ int main(int argc, string argv[])
 {
     string key = argv[1];
     int key_len = strlen(key);
+    if (key == ' ')
+        printf("Usage: ./substitution key\n");
     if (key_len < 26)
-            printf("Key must contain 26 characters.\n");
+        printf("Key must contain 26 characters.\n");
     else
     {
         for(int i = 0; i < key_len; i++)
         {
-            
+
         }
     }
 
-    printf("%s\n",argv[1]);
+    //printf("%s\n",argv[1]);
 }

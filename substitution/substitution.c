63b84e5beb9262d1ac30dcb7f8f05268da33db36
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-06 11:03:31 +0000

    Wed, Nov 6, 2024, 2:03 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -66,6 +66,7 @@ void encryption(string plain, string key)
     for (int i = 0, int len = srtlen(plain); i < len; i++)
         if (isupper(plain[i]))
             for (int j= 0, int len2 = strlen(key);j < len2; j++)
+            
 }
 
 //void decryption(string cypher) { string a = "v";}

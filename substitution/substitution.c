e909918a2bfe785b30312868b218599a62cedc5e
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-06 11:16:37 +0000

    Wed, Nov 6, 2024, 2:16 PM +03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -67,7 +67,7 @@ void encryption(string plain, string key)
 {
 
     for (int i = 0, len = strlen(plain); i < len; i++)
-        if ((isupper(plain[i])) && (plain[i] != '\0'))
+        if (isupper(plain[i]))
             for (int j= 0, len2 = strlen(key); j < len2; j++)
             plain[i] = key[j];
     printf("ciphertext: %s\n", plain);

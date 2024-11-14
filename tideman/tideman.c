10a622b86e15d35406fcdd3225866ee2898f18fe
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-14 08:51:30 +0000

    Thu, Nov 14, 2024, 11:51 AM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -115,7 +115,9 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
-    for (int i = 0; i < )
+    for (int i = 0; i < candidate_count; i++)
+        for (int j = i + 1; j < candidate_count; j++)
+        
     return;
 }
 

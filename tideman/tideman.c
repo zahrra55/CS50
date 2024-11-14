ad55f55cb3aa0c7d94e6c24aa1bd41ecf7e9eae7
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-14 08:45:56 +0000

    Thu, Nov 14, 2024, 11:45 AM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -100,7 +101,7 @@ bool vote(int rank, string name, int ranks[])
 {
     // TODO
     for (int i = 0;i < candidate_count; i++)
-        if (cmpstr(candidates[i], name) == 0)
+        if (strcmp(candidates[i], name) == 0)
         return true;
     return false;
 }

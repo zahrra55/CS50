dbbb0879bcc4e5c2b4a5b4c644370be9b1387554
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-18 11:21:49 +0000

    Mon, Nov 18, 2024, 2:21 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -118,8 +118,6 @@ void record_preferences(int ranks[])
     for (int i = 0; i < candidate_count; i++)
         for (int j = i + 1; j < candidate_count; j++)
                 preferences[ranks[i]][ranks[j]]++;
-
-    return;
 }
 
 // Record pairs of candidates where one is preferred over the other

d2267eadc9f0ab488f088edc1338a3d2a690578d
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-18 11:37:51 +0000

    Mon, Nov 18, 2024, 2:37 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -126,14 +126,22 @@ void add_pairs(void)
     // TODO
      for (int i = 0; i < candidate_count; i++)
         for (int j = i + 1; j < candidate_count; j++)
+        {
             if (preferences[i][j] > preferences[j][i])
             {
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
                 pair_count++;
             }
-            //if (preferences[i][j] == preferences[j][i])
-                //return;
+            else if (preferences[i][j] < preferences[j][i])
+                {
+                    pairs[pair_count].winner = j;
+                    pairs[pair_count].loser = i;
+                    pair_count++;
+                }
+
+        }
+
     return;
 }
 

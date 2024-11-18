3eca2cca58107cb4ac4dc4462602981e08e946fa
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-18 11:21:11 +0000

    Mon, Nov 18, 2024, 2:21 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -117,7 +117,6 @@ void record_preferences(int ranks[])
     // TODO
     for (int i = 0; i < candidate_count; i++)
         for (int j = i + 1; j < candidate_count; j++)
-           // if (ranks[i] > ranks[j])
                 preferences[ranks[i]][ranks[j]]++;
 
     return;

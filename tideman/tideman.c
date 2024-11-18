bbdc1f9a91c316dcd3d691d384e15e22678f7211
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-18 10:26:17 +0000

    Mon, Nov 18, 2024, 1:26 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -164,7 +164,7 @@ void lock_pairs(void)
     // TODO
     for (int i = 0; i < pair_count; i++)
     if (!cycle(pairs[i].winner,pairs[i].loser))
-        locked[pairs[i.winner]][pairs[i].loser] = true;
+        locked[pairs[i].winner][pairs[i].loser] = true;
     return;
 }
 

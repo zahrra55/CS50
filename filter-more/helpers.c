a429ea6cf36112ec0ffdbc12dc6512ec842aa805
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-30 10:25:41 +0000

    Sat, Nov 30, 2024, 1:25 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -16,8 +16,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
     int half = width / 2;
-    for (int i = 0; i < height; i++)
-        for (int j = 0; j < width; j++)
+    for (int i = 0; i < height - 1; i++)
+        for (int j = 0; j < width - 1; j++)
         {
             if (i > half)
             image[i][j] = image[i][(half + 1) - j];

0206ccb463d1165b84c5b1d4da089f3adba74365
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-30 06:17:26 +0000

    Sat, Nov 30, 2024, 9:17 AM +03:00

--- volume/volume.c
+++ volume/volume.c
@@ -35,9 +35,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-
-    fread(output,HEADER_SIZE,1,input);
-        fwrite(output,HEADER_SIZE,1,input);
+    uint8_t header[HEADER_SIZE];
+    fread(header,HEADER_SIZE,1,input);
+    fwrite(header,HEADER_SIZE,1,output);
 
 
     // TODO: Read samples from input file and write updated data to output file

3a81d40319f33c5fccbcdc532c6da12ecb57bc5f
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-30 10:33:38 +0000

    Sat, Nov 30, 2024, 1:33 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -18,7 +18,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
         for (int j = 0; j < width - 1; j++)
         {
+            RGBTRIPLE temp = image[i][j];
             image[i][j] = image[i][width - j - 1];
+            image[i][width - j - 1] = temp;
         }
 }
 

cd2cb98a0dde2b2265853ed0e248ec97245f834b
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-11-30 05:54:07 +0000

    Sat, Nov 30, 2024, 8:54 AM +03:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,9 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    FILE *r = fread(input);
     
+
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files

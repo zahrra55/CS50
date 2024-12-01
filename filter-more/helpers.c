f1f30d4837dd8ef67bdd0a269c9744ac56bfdf9d
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-01 06:53:46 +0000

    Sun, Dec 1, 2024, 9:53 AM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -104,10 +104,14 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
+            temp[i][j].rgbtRed *= Gx;
+            temp[i][j].rgbtGreen *= Gx;
+            temp[i][j].rgbtBlue *= Gx;
+
             // Calculate the average and assign to temp
-            temp[i][j].rgbtRed *= round((float)Gx);
-            temp[i][j].rgbtGreen *= round((float)Gx);
-            temp[i][j].rgbtBlue *= round((float)Gx);
+            temp[i][j].rgbtRed = round((float)sumRed / count);
+            temp[i][j].rgbtGreen = round((float)sumGreen / count);
+            temp[i][j].rgbtBlue = round((float)sumBlue / count);
 
         }
     }

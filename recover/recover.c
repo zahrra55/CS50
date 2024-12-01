e1e8b97b08aa1035731385f194feadb0c65be531
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-01 20:23:27 +0000

    Sun, Dec 1, 2024, 11:23 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,6 +4,7 @@
 int main(int argc, char *argv[])
 {
     //TODO: Open memory card
+    FILE *f = fopen("file.jpeg", "r");
     //TODO: Look for a begining for JPEG
     //TODO: Open a new JPEG file
     //TODO: Write 512 bytes until a new JPEG is found

71f639a03636bec6952a99bfcd021048ec3a97ca
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-01 11:56:31 +0000

    Sun, Dec 1, 2024, 2:56 PM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -7,7 +7,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for (int i = 0; i < height; i++)
         for (int j = 0; j < width; j++)
         {
-            int avarage = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3);
+            int avarage = round((image[i][j].rgbtRed + image[i][j].rgbtGreen + image[i][j].rgbtBlue) / 3.0);
             image[i][j].rgbtRed = image[i][j].rgbtGreen = image[i][j].rgbtBlue = avarage;
         }
 

39077a0049661231d5541a9d91db413b512c7a6e
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-01 06:46:23 +0000

    Sun, Dec 1, 2024, 9:46 AM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -76,5 +76,48 @@ void edges(int height, int width, RGBTRIPLE image[height][width])
 {
     int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
-    
+
+
+    RGBTRIPLE temp[height][width];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            //sumRed = sumGreen = sumBlue = count = 0; // Reset sums for each pixel
+
+            for (int ni = -1; ni < 2; ni++)
+            {
+                for (int nj = -1; nj < 2; nj++)
+                {
+                    int nei = i + ni;
+                    int nej = j + nj;
+
+                    // Check if neighbor pixel is valid
+                    if (nei >= 0 && nei < height && nej >= 0 && nej < width)
+                    {
+                        sumRed += image[nei][nej].rgbtRed;
+                        sumGreen += image[nei][nej].rgbtGreen;
+                        sumBlue += image[nei][nej].rgbtBlue;
+                        count++;
+                    }
+                }
+            }
+
+            // Calculate the average and assign to temp
+            temp[i][j].rgbtRed = round((float)sumRed / count);
+            temp[i][j].rgbtGreen = round((float)sumGreen / count);
+            temp[i][j].rgbtBlue = round((float)sumBlue / count);
+        }
+    }
+
+    // Copy the blurred image back to the original
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+
 }

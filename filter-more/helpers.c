3f0a1481591d1cbce698b2213f43c769ed599081
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-01 06:41:54 +0000

    Sun, Dec 1, 2024, 9:41 AM +03:00

--- filter-more/helpers.c
+++ filter-more/helpers.c
@@ -74,6 +74,7 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    int Gx[][] = {-1, 0, 1, -2, 0, 2, -1, 0, 1};
-    //int Gy[3][3] = {-1, -2, -1, 0, 0, 0, 1, 2, 1};
+    int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
+    int Gy[3][3] = {{-1, -2, -1}, {0, 0, 0}, {1, 2, 1}};
+    
 }

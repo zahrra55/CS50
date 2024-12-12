8c40e7a13a9c6111d7663e3423305a5bf1066014
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-12 13:23:08 +0000

    Thu, Dec 12, 2024, 4:23 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -9,9 +9,11 @@ int main(int argc, char *argv[])
     //TODO: Open a new JPEG file
     //TODO: Write 512 bytes until a new JPEG is found
     //TODO: Stop at end of file
-    /*e first three bytes of JPEGs are
-0xff 0xd8 0xff
-from first byte to third byte, left to right. The fourth byte, meanwhile,
-is either 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec,
-0xed, 0xee, or 0xef. Put another way, the fourth byte’s first four bits are 1110.*/
+    /*
+    the first three bytes of JPEGs are
+    0xff 0xd8 0xff
+    from first byte to third byte, left to right. The fourth byte, meanwhile,
+    is either 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec,
+    0xed, 0xee, or 0xef. Put another way, the fourth byte’s first four bits are 1110.
+    */
 }

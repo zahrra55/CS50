52b41d32074922bf3cceac82527343f63241233c
Zahraa <111491060+zahrra55@users.noreply.github.com>
2024-12-31 17:20:18 +0000

    Tue, Dec 31, 2025, 8:20 PM +03:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,19 +1,66 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 512
+
 int main(int argc, char *argv[])
 {
-    //TODO: Open memory card
-    FILE *f = fopen("file.jpeg", "r");
-    //TODO: Look for a begining for JPEG
-    //TODO: Open a new JPEG file
-    //TODO: Write 512 bytes until a new JPEG is found
-    //TODO: Stop at end of file
-    /*
-    the first three bytes of JPEGs are
-    0xff 0xd8 0xff
-    from first byte to third byte, left to right. The fourth byte, meanwhile,
-    is either 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec,
-    0xed, 0xee, or 0xef. Put another way, the fourth byte’s first four bits are 1110.
-    */
+    // Accept a single command-line argument
+    if (argc != 2)
+    {
+        printf("Usage: ./recover FILE\n");
+        return 1;
+    }
+    // Open the memory card
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
+    {
+        printf("couldn't open the file..\n");
+        return 1;
+    }
+    printf("the file is opened!\n");
+
+    // Create a buffer for a block of data
+    uint8_t buffer[BUFFER_SIZE];
+    char fname[8];
+    FILE *img = NULL;
+    int c = 0;
+
+    // While there's still data left to read from the memory card
+    while (fread(buffer, 1, BUFFER_SIZE, card) == BUFFER_SIZE)
+    {
+        // Create JPEGs from the data
+        if ((buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff) &&
+            ((buffer[3] & 0xf0) == 0xe0))
+        {
+            if (img != NULL)
+            {
+                fclose(img);
+            }
+
+            // Open a new JPEG file
+            sprintf(fname, "%03i.jpg", c++);
+
+            img = fopen(fname, "w");
+
+            if (img == NULL)
+            {
+                fprintf(stderr, "Error: Could not open file %s\n", fname);
+                fclose(card);
+                return 1; // Or appropriate error handling
+            }
+        }
+        if (img != NULL)
+        {
+            fwrite(buffer, 1, BUFFER_SIZE, img);
+        }
+    }
+
+    if (img != NULL)
+    {
+        fclose(img);
+    }
+
+    fclose(card);
 }

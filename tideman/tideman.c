be6f8356a778e78020fad7d6793f6dfbda47a488
Zahraa <111491060+zahrra55@users.noreply.github.com>
2025-02-14 13:50:52 +0000

    Fri, Feb 14, 2025, 4:50 PM +03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,7 +100,7 @@ int main(int argc, string argv[])
 // Update ranks given a new vote
 bool vote(int rank, string name, int ranks[])
 {
-    // TODO
+
     for (int i = 0; i < candidate_count; i++)
         if (strcmp(candidates[i], name) == 0)
         {

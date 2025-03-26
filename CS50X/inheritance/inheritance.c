2eac1f3c88d80ca5ed65e0c6004cd2bcf2156383
Zahraa <111491060+zahrra55@users.noreply.github.com>
2025-03-26 21:19:17 +0300

    Initial commit of CS50X files

--- CS50X/inheritance/inheritance.c
+++ CS50X/inheritance/inheritance.c
@@ -38,7 +38,7 @@ int main(void)
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
-    // TODO: Allocate memory for new person
+    // Allocate memory for new person
     person *p = (person *) malloc(sizeof(person));
 
     // If there are still generations left to create
@@ -48,11 +48,11 @@ person *create_family(int generations)
         person *parent0 = create_family(generations - 1);
         person *parent1 = create_family(generations - 1);
 
-        // TODO: Set parent pointers for current person
+        // Set parent pointers for current person
         p->parents[0] = parent0;
         p->parents[1] = parent1;
 
-        // TODO: Randomly assign current person's alleles based on the alleles of their parents
+        // Randomly assign current person's alleles based on the alleles of their parents
         p->alleles[0] = parent0->alleles[random() % 2];
         p->alleles[1] = parent1->alleles[random() % 2];
     }
@@ -60,16 +60,16 @@ person *create_family(int generations)
     // If there are no generations left to create
     else
     {
-        // TODO: Set parent pointers to NULL
+        // Set parent pointers to NULL
         p->parents[0] = NULL;
         p->parents[1] = NULL;
 
-        // TODO: Randomly assign alleles
+        // Randomly assign alleles
         p->alleles[0] = random_allele();
         p->alleles[1] = random_allele();
     }
 
-    // TODO: Return newly created person
+    // Return newly created person
     return p;
 }
 
@@ -81,10 +81,10 @@ void free_family(person *p)
     {
         return;
     }
-    // TODO: Free parents recursively
+    // Free parents recursively
     free_family(p->parents[0]);
     free_family(p->parents[1]);
-    // TODO: Free child
+    // Free child
     free(p);
 }
 

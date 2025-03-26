abf479ab91847958a13a36526a47564000dc945b
Zahraa <111491060+zahrra55@users.noreply.github.com>
2025-03-26 20:09:16 +0300

    Delete CS50X directory

--- CS50X/movies/9.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT people.name
-FROM movies
-JOIN stars ON movies.id = stars.movie_id
-JOIN people ON people.id = stars.person_id
-WHERE movies.year = 2004
-ORDER BY people.birth;
-
--- CS50X/world/hello.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-// this is my first c try
-int main(void)
-{
-    printf("hello, world");
-}
